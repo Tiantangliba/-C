afb9f88d313ec13bc457d193167dcbb948de12c8
tiantangliba <1366392733@qq.com>
2023-01-10 13:52:01 +0800

    日常学习

---
+++ 10.29/10.29/Seqlist.h
@@ -0,0 +1,30 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+typedef int SLDateType;
+
+typedef struct SeqList
+{
+	SLDateType* a;
+	size_t size;
+	size_t capacity; // unsigned int
+}SeqList; 
+
+// 对数据的管理:增删查改 
+void SeqListInit(SeqList* ps);//初始化
+void SeqListDestroy(SeqList* ps);//销毁
+void SeqListPrint(SeqList* ps);//打印
+void SeqListPushBack(SeqList* ps, SLDateType x);//尾插
+void SeqListPushFront(SeqList* ps, SLDateType x);//头插
+void SeqListPopFront(SeqList* ps);//
+void SeqListPopBack(SeqList* ps);//尾删
+
+// 顺序表查找
+int SeqListFind(SeqList* ps, SLDateType x);
+// 顺序表在pos位置插入x
+void SeqListInsert(SeqList* ps, size_t pos, SLDateType x);
+// 顺序表删除pos位置的值
+void SeqListErase(SeqList* ps, size_t pos);
